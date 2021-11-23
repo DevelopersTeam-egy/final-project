dc8583525d3008812eac6e8e8b02ce085464710c
mosadek777 <mohmedmahmoudsadek7@gmail.com>
2021-11-23 18:02:07 +0200

    ADD About

---
+++ about/js/about.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        margin:20,
+        loop:true,
+        center:true,
+        autoplay:true
+    });
+   
+  });
+  
+
+
+
